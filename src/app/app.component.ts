7980883f2240f123c46a482ffbf2740b33b90db6
Sourav2707 <smartsourav33@gmail.com>
2023-07-13 14:41:02 +0530

    UC3 Adding bridgelabz logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ which is used to output the data from a TypeScript code to HTML template (view).
  */
 export class AppComponent {
   title = 'HelloApp';
+  imgUrl = "https://www.educationworld.in/wp-content/uploads/2020/06/BridgeLabz-2.jpg";
   ngOnInit(): void {
     this.title = "Hello from Bridgelabz"
   }
