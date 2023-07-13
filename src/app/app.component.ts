cbbb95164d614ba31d7718aac0a65285cee1b413
Sourav2707 <smartsourav33@gmail.com>
2023-07-13 14:35:34 +0530

    UC2 ability to display hello message using one way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+/**
+ * String Interpolation is a one-way databinding technique 
+which is used to output the data from a TypeScript code to HTML template (view).
+ */
 export class AppComponent {
   title = 'HelloApp';
+  ngOnInit(): void {
+    this.title = "Hello from Bridgelabz"
+  }
 }
