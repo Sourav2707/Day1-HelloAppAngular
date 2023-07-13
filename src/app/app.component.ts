0194b09c3b85c43b681f894f436d6efef45f176e
Sourav2707 <smartsourav33@gmail.com>
2023-07-13 15:06:21 +0530

    UC6 validate the user input name using regex

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,11 +14,22 @@ export class AppComponent {
   imgUrl = "https://www.educationworld.in/wp-content/uploads/2020/06/BridgeLabz-2.jpg";
   url = "https://www.bridgelabz.com/";
   userName: string = "";
+  nameError: string = "";
+
   ngOnInit(): void {
     this.title = "Hello from Bridgelabz"
   }
   onClick($event: any) {
     console.log("Save button is clicked!", $event);
     window.open(this.url, "_blank");
+  }
+  onInput($event : any) {
+    console.log("Change Event Occurred", $event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.userName)) {
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is invalid"
   }
 }
