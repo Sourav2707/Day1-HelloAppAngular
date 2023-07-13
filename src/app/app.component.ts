6ccfe672df99cc70766c2be2cf114a94e6c4976c
Sourav2707 <smartsourav33@gmail.com>
2023-07-13 14:48:30 +0530

    UC4 ability to launch bridgelabs website when clicking logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,12 @@ which is used to output the data from a TypeScript code to HTML template (view).
 export class AppComponent {
   title = 'HelloApp';
   imgUrl = "https://www.educationworld.in/wp-content/uploads/2020/06/BridgeLabz-2.jpg";
+  url = "https://www.bridgelabz.com/";
   ngOnInit(): void {
     this.title = "Hello from Bridgelabz"
+  }
+  onClick($event: any) {
+    console.log("Save button is clicked!", $event);
+    window.open(this.url, "_blank");
   }
 }
